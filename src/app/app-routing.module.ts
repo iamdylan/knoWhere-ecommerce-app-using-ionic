cc231dc30be35f6b06f7ce545044260d23b0b30d
iamdylan <43369528+iamdylan@users.noreply.github.com>
2019-03-26 22:17:23 +0530

    Added page for individual product details and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsByCategoryPage } from './products-by-category/products-by-category.page';
 import { HomePage } from './home/home.page';
+import { ProductDetailsPage } from './product-details/product-details.page';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePage },
-  { path: 'products/:category', component: ProductsByCategoryPage }
+  { path: 'products/:category', component: ProductsByCategoryPage },
+  { path: 'product-details/:product', component: ProductDetailsPage }
 ];
 
 @NgModule({
