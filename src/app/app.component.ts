f250fc11e96732ddf9bdff28ff1f3f7d12ad7e32
iamdylan <43369528+iamdylan@users.noreply.github.com>
2019-06-03 10:59:24 +0530

    Restructured App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,18 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { MenuPage } from './menu/menu.page';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
+
+  rootPage: any = MenuPage;
 
   constructor(
     private platform: Platform,
