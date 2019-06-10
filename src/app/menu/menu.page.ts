534e1c54dc41e4725c0d664e4952ef87ff8503c3
iamdylan <43369528+iamdylan@users.noreply.github.com>
2019-06-10 21:47:55 +0530

    Added Categories on sidemenu

--- src/app/menu/menu.page.ts
+++ src/app/menu/menu.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HomePage } from '../home/home.page';
+import * as WC from 'woocommerce-api';
 
 @Component({
   selector: 'app-menu',
@@ -8,8 +9,39 @@ import { HomePage } from '../home/home.page';
 })
 export class MenuPage implements OnInit {
   homePage: any;
+  WooCommerce: any;
+  categories: any[];
+
   constructor() { 
     this.homePage = HomePage;
+    this.categories = [];
+
+    this.WooCommerce =  WC({
+      url: "http://localhost/dashboard/wordpress",
+      consumerKey: "ck_b137f07c8316ede0376d58741bf799dada631743",
+      consumerSecret: "cs_300fb32ce0875c45a2520ff860d1282a8891f113",
+      wpAPI: true,
+      version: 'wc/v3'
+    });
+
+    this.WooCommerce.getAsync("products/categories").then((data) => {
+      console.log(JSON.parse(data.body));
+
+      let temp: any[] = JSON.parse(data.body);
+
+      for( let i = 1; i < temp.length; i ++){
+        if(temp[i].parent == 0){
+          this.categories.push(temp[i]);
+        }
+      }
+      if(temp[0].parent == 0){
+        this.categories.push(temp[0]);
+      }
+    }, (err)=> {
+      console.log(err)
+    })
+
+    
   }
 
   ngOnInit() {
