daaa55b351797dbbe6242156f833d9453af8abfa
iamdylan <43369528+iamdylan@users.noreply.github.com>
2019-06-26 22:21:42 +0530

    Updated Cart UI and logic

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cart',
@@ -6,8 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.page.scss'],
 })
 export class CartPage implements OnInit {
+  cartItems: any[] = [];
+  total: any;
+  showEmptyCartMessage: boolean = false;
 
-  constructor() { }
+  constructor(public storage: Storage, public modalCtrl: ModalController) { 
+    this.total = 0.0;
+    
+    this.storage.ready().then(()=>{
+      this.storage.get("cart").then( (data)=>{
+        this.cartItems = data;
+        console.log(this.cartItems);
+
+        if(this.cartItems.length > 0){
+          this.cartItems.forEach( (item, index)=> {
+            this.total = this.total + (item.product.price * item.qty)
+          })
+        } else {
+          this.showEmptyCartMessage = true;
+        }
+      })
+    })
+  }
+
+  removeFromCart(item, i){
+    let price = item.product.price;
+    let qty = item.qty;
+
+    this.cartItems.splice(i, 1);
+    this.storage.set("cart", this.cartItems).then( ()=> {
+      this.total = this.total - (price * qty);
+    });
+
+    if(this.cartItems.length == 0){
+      this.showEmptyCartMessage = true;
+    }
+  }
+
+  closeModal(){
+    this.modalCtrl.dismiss();
+  }
+
+  checkOut(){
+      // let data=null;
+      // this.storage.set("cart", data).then(() => {
+      //   console.log("Cart Updated");
+      //   console.log(data);
+      // })
+  }
 
   ngOnInit() {
   }
