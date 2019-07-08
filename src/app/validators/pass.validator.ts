6ffc1734b864217aff7b06e6dabeaef9ec9ed803
iamdylan <43369528+iamdylan@users.noreply.github.com>
2019-07-08 18:38:56 +0530

    Created custom validator for password confirmation

--- src/app/validators/pass.validator.ts
+++ src/app/validators/pass.validator.ts
@@ -1,31 +1,11 @@
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 export class PasswordValidator {
 
-    static validPassword(formGroup: FormGroup) {
-    let value;
-    let valid = true;
-    for (let key in formGroup.controls) {
-      if (formGroup.controls.hasOwnProperty(key)) {
-        let control: FormControl = <FormControl>formGroup.controls[key];
-
-        if (value === undefined) {
-          value = control.value
-        } else {
-          if (value !== control.value) {
-            valid = false;
-            break;
-          }
-        }
-      }
-    }
-
-    if (valid) {
-      return null;
-    }
-
-    return {
-      validPassword: true
-    };
+  static validPassword(group: FormGroup) {
+  let pass = group.controls.password.value;
+  let confirmPass = group.controls.conf_password.value;
+  
+  return pass === confirmPass ? null : { noMatch: true }
   }
 }
