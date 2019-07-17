83edf98e0a0ddfab312068e49235fc9e06373e2a
iamdylan <43369528+iamdylan@users.noreply.github.com>
2019-07-17 19:35:34 +0530

    Updated Menu UI & added route to Home Page

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -169,35 +169,6 @@ setBillingToShipping(){
     });
   }
 }
-
-userValidation=()=>{
-  console.log("user running")
-  let WooCommerce =  WC({
-      url: "http://localhost/dashboard/wordpress",
-      consumerKey: "ck_b137f07c8316ede0376d58741bf799dada631743",
-      consumerSecret: "cs_300fb32ce0875c45a2520ff860d1282a8891f113",
-      wpAPI: true,
-      version: 'wc/v3'
-    });
-
-    WooCommerce.getAsync("customers/?username=" + this.reactForm.value.username).then((data) => {
-      console.log(JSON.parse(data.body));
-      console.log(JSON.parse(data.body).length);
-
-      this.ngZone.run(() => {
-        this.userGood = false;
-      });
-
-      Array.prototype.some.call(JSON.parse(data.body), (item)=>{
-        if (item.username == this.reactForm.value.username){
-          this.ngZone.run(() => {
-            this.userGood = true;
-         });
-         return (item.username == this.reactForm.value.username);
-        }
-      })
-    });
-}
   
 signup(){
     let WooCommerce =  WC({
