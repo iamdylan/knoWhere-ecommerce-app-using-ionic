244ac74e1f1e9fee4527e8b269645d5cc45eda2a
iamdylan <dylan.marvel@gmail.com>
2019-11-15 10:49:14 +0530

    Performance tweak by minimizing layout changes & eager-loading home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NeedAuthGuard } from './login/auth.guard';
 import { CheckoutGuard } from './checkout/checkout.guard';
+import { HomePage } from './home/home.page';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'home', component: HomePage },
   { path: 'products/:category', loadChildren: './products-by-category/products-by-category.module#ProductsByCategoryModule' },
   { path: 'product-details/:product', loadChildren: './product-details/product-details.module#ProductDetailsModule' },
   { path: 'cart', loadChildren: './cart/cart.module#CartPageModule', canActivate: [NeedAuthGuard] },
